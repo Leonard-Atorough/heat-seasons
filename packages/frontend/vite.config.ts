0510369461d1a031f658266ec8e7ce9a6d0c8fe5
Leonard Atorough <leotorough@gmail.com>
2026-03-05 22:45:38 +0000

    refactor: update alias in vite.config.ts to remove leading '@' for consistency

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@src": resolve(__dirname, "./src"),
+      "src": resolve(__dirname, "./src"),
     },
   },
   server: {
